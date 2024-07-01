1dc602833b77e87866e73f94dce5bb6469540d49
gflahaut <flahaut.gwendoline@gmail.com>
2024-07-02 00:26:39 +0200

    #1 init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import NotFound from './pages/NotFound/NotFound';
+import Details from './pages/Details/Details'; // Import du composant Details
 import './App.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/details/:id" element={<Details/>} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   );
-}
+};
 
 export default App;
