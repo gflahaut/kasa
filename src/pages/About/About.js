cd7649c9703420d58ff270ca08582c35112ada6e
gflahaut <flahaut.gwendoline@gmail.com>
2024-07-02 22:10:52 +0200

    About

---
+++ src/pages/About/About.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import AboutBanner from '../../components/Banners/Banners';
+import Collapse from '../../components/Collapse/Collapse';
+import aboutData from '../../assets/data/about.json'; // Importez les données JSON
+import './About.scss';
+
+export default function About() {
+  return (
+    <div className="about-page">
+      <AboutBanner />
+      <div className="about-content">
+        {aboutData.map((rule, id) => (
+          <Collapse
+            key={id}
+            aboutTitle={rule.aboutTitle}
+            aboutText={rule.aboutText}
+            aboutStyle="about-style"
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
